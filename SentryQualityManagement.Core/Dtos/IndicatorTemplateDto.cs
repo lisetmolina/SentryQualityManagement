1d2b9557fb5e2ddcbccccae55445b6ffb792169a
Lima <vl9369872@gmail.com>
2021-08-24 10:06:18 -0500

    Implementacion en Dto, IndicatorId.

--- SentryQualityManagement.Core/Dtos/IndicatorTemplateDto.cs
+++ SentryQualityManagement.Core/Dtos/IndicatorTemplateDto.cs
@@ -6,7 +6,7 @@ namespace SentryQualityManagement.Core.Dtos
 {
     public class IndicatorTemplateDto
     {
-      
+        public int IndicatorId { get; set; }
         public string ElementName { get; set; }
         public int ElementValue { get; set; }
         public DateTime ElementDate { get; set; }
